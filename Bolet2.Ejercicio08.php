6008d5075a06f7384d1167391ec53c9f559cefe0
AvilaDelgadoAntonio <72385850+AvilaDelgadoAntonio@users.noreply.github.com>
2021-11-02 00:40:33 +0100

    Delete Bolet2.Ejercicio08.php

--- Bolet2.Ejercicio08.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*Realizar un programa que calcule la letra del DNI. El método consiste en dividir el número del DNI entre 23 y al resto de la división se le hace corresponder con una letra
-del abecedario según la siguiente tabla.*/
-$numero = $_POST["numero"];
-$letras = array("T","R","W","A","G","M","Y","F","P","D","X","B","N","J","Z","S","Q","V","H","L","C","K","E","T");
-$indice = ($numero%23);
-echo "<br>$numero = $letras[$indice]<br>";
-?>
