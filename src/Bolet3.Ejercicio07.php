0db89c5b1f24897c16814a30e9d853d51e42ece0
AvilaDelgadoAntonio <72385850+AvilaDelgadoAntonio@users.noreply.github.com>
2021-11-04 10:19:12 +0100

    Update Bolet3.Ejercicio07.php

--- src/Bolet3.Ejercicio07.php
+++ src/Bolet3.Ejercicio07.php
@@ -1,15 +1,33 @@
 <?php
 /*A partir de los ficheros Capitales.txt y Países.txt. Realizar un programa que
-muestre un país y le pida al usuario que seleccione la capital. El programa le
+muestre un país (de forma aleatoria) y le pida al usuario, en un formulario, que seleccione la capital. El programa le
 dirá si ha acertado o no. En los ficheros se corresponde el primer país con la
 primera capital, el segundo país con la segunda capital,...*/
-$na=48;
-$paises=file("paises.txt");
-$capitales=file("capitales.txt");
-$na=sizeof($paises);
-for ($i = 0; $i < $na; $i++) 
 
-{
-	echo "<br>$paises[$i] - $capitales[$i]";
-	}
+
+$paises=file("paises.txt");
+$cifra=mt_rand(1,48);
+echo "<br>Indica la capital de: $paises[$cifra]";
+<html>
+<body>
+<form action="Bolet3.Ejercicio07_IN PROGRESS.php" method="post">
+
+<select name="capital">
+
+<option>Paris</option>
+
+<option>Madrid</option>
+
+<option>Londres</option>
+
+</select>
+ <p><input type="submit" /></p>
+</form>
+</body>
+</html>
+if ($capitales[$cifra]==$_POST["capital"]) {
+	echo "correcta";
+}else{
+	echo "INcorrecta";
+}
 ?>
